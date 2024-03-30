b9d58f173aa7307686357c7378c3ae2130483bfb
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-03-30 18:58:23 +0300

    dz_13.2

--- utils/class_category.py
+++ utils/class_category.py
@@ -1,10 +1,13 @@
+from utils.class_product import Product
+
+
 class Category:
     """
     Класс категорий продуктов.
     """
     name: str
     description: str
-    products: list
+    __products: list
     number_of_categories: int  # общее количество категорий
     number_of_unique_products: int  # общее количество уникальных продуктов,
     # не учитывая количество в наличии
@@ -20,12 +23,29 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.number_of_categories += 1
         Category.number_of_unique_products += len(products)
 
-    def __repr__(self):
-        return (f'Название товара: {self.name}.\n'
-                f'Описание товара: {self.description}.\n'
-                f'Список товаров: {self.products}.')
+    def app_products(self, product):
+        """
+        Метод для добавления товаров в список.
+        """
+        for x in product:
+            self.__products.append(x)
+
+    @property
+    def products(self):
+        """
+        Преобразуем список товаров в необходимый формат.
+        """
+        list_product = Product
+        return (f'{list_product.name}, {list_product.price} руб. '
+                f'Остаток: {list_product.quantity} шт.')
+
+
+def __repr__(self):
+    return (f'Название товара: {self.name}.\n'
+            f'Описание товара: {self.description}.\n'
+            f'Список товаров: {self.__products}.')
--- utils/class_product.py
+++ utils/class_product.py
@@ -5,23 +5,81 @@ class Product:
     name: str
     description: str
     price: float
-    quantity_in_stock: int
+    quantity: int
+
+    list_product = []  # список продукта с полным его описанием
 
     def __init__(self, name: str, description: str, price: float,
-                 quantity_in_stock: int):
+                 quantity: int):
         """
         :param name: название продукта.
-        :param description: описание продукта.
-        :param price: цена продукта.
-        :param quantity_in_stock: количество продуктов в наличии.
+        :param description: Описание продукта.
+        :param price: Цена продукта.
+        :param quantity: Количество продуктов в наличии.
         """
         self.name = name
         self.description = description
-        self.price = price
-        self.quantity_in_stock = quantity_in_stock
+        self.__price = price
+        self.quantity = quantity
+
+        Product.list_product.append(self)
+
+    @classmethod
+    def product_creation(cls, new_product: dict):
+        """
+        Метод, который создает товар и возвращает объект.
+        Который можно добавлять в список товаров.
+        :return: Новый товар.
+        """
+
+        name = new_product['name']
+        description = new_product['description']
+        price = new_product['price']
+        quantity = new_product['quantity']
+        for product in Product.list_product:
+            if product.name == name and product.description == description:
+                product.quantity += quantity
+                if product.price < price:
+                    product.price = price
+                return product
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """
+        Декоратор для возращения цены продукта.
+        :return: Цена продукта.
+        """
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """
+        Сеттер для подтверждения корректности введенной цены и ее понижения
+        при необходимости.
+        :param new_price: Новая цена.
+        """
+        if new_price <= 0:
+            print('Цена введена некорректно')
+
+        if new_price < self.__price:
+            user_input = input('Подтвердите цену:\n y - подтвердить'
+                               '\n n - отменить').lower().strip()
+            if user_input == 'y' or 'yes':
+                self.__price = new_price
+        else:
+            self.__price = new_price
+
+    @price.deleter
+    def price(self):
+        """
+        Удаляем цену продукта.
+        :return: None
+        """
+        del self.__price
 
     def __repr__(self):
         return (f'Название продута: {self.name}.\n'
                 f'Описание продукта: {self.description}.'
                 f'Цена продукта: {self.price}.\n'
-                f'Количество продуктов в наличии: {self.quantity_in_stock}.')
+                f'Количество продуктов в наличии: {self.quantity}.')
