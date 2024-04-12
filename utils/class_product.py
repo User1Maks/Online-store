0d792bba79c156c27f75fea666b34bf1701e8385
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-04-12 20:13:32 +0300

    dz_15.2

--- utils/class_product.py
+++ utils/class_product.py
@@ -4,8 +4,11 @@ from abc import ABC, abstractmethod
 class Assortment(ABC):
     """
      Абстрактный метод для онлайн магазина. Содержит общие методы
-     для продуктов.
-    """
+     для продуктов. Является родительским классом для классов наследников:
+     - Product наследуется напрямую от класса Assortment;
+     - Smartphone наследуется от класса Product;
+     - LawnGrass наследуется от класса Product.
+     """
 
     @abstractmethod
     def __str__(self):
@@ -18,12 +21,83 @@ class Assortment(ABC):
     @abstractmethod
     def __add__(self, other):
         """
-        Метод для подсчета полной стоимости продуктов категории на складе.
+        Метод для подсчета полной стоимости продуктов категории на складе,
+        которая будет складываться из цены товара, умноженное на количество
+        товара на складе.
         """
         pass
 
+    @abstractmethod
+    def __repr__(self):
+        """
+        Метод для отладки каждого класса
+        """
+        pass
 
-class Product(Assortment):
+    @classmethod
+    @abstractmethod
+    def product_creation(cls, new_product: dict) -> object:
+        """
+        Абстрактный метод для создания товара и возвращения объекта,
+        который можно добавлять в список товаров.
+        :return: Объект (новый товар).
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def price(self):
+        """
+        Декоратор для извлечения цены из защищенного атрибута
+        экземпляра класса "self.__price", который по принципу наследования
+        присутствует в каждом дочернем классе.
+        """
+        pass
+
+    @price.setter
+    @abstractmethod
+    def price(self, new_price) -> None:
+        """
+        Абстрактный метод для подтверждения корректности введенной цены
+        или ее понижения при необходимости.
+        :param new_price: Новая цена.
+        """
+        pass
+
+    @price.deleter
+    @abstractmethod
+    def price(self) -> None:
+        """
+        Абстрактный метод для удаления цены продукта.
+        :return: None
+        """
+
+
+class MixinRepr:
+    """
+     Класс Mixin для добавления каждому классу вывод информации в консоль о том,
+     что был создан объект.
+    """
+
+    def printing(self, init):
+        """
+        Декоратор "printing", который принимает функцию "init"
+        """
+
+        def print_info(*args, **kwargs):
+            """
+            Функция для вызова функции "printing" с аргументами *args и **kwargs
+            :param args: Позиционный аргумент.
+            :param kwargs: Аргументы по ключам.
+            """
+            result = init(*args, **kwargs)
+            print(repr(self))
+            return result
+
+        return print_info
+
+
+class Product(Assortment, MixinRepr):
     """
     Класс продуктов. Имеет два класса наследника: Smartphone и LawnGrass.
     """
@@ -48,6 +122,7 @@ class Product(Assortment):
         self.quantity = quantity
 
         Product.list_product.append(self)
+        super().__init__()
 
     @classmethod
     def product_creation(cls, new_product: dict) -> object:
@@ -119,6 +194,7 @@ class Product(Assortment):
         return f'{self.name}, {self.__price} руб. Остаток: {self.quantity}'
 
     def __repr__(self) -> str:
+        """ Метод для отладки категории Product"""
         return (f'{self.__class__.__name__}:\n'
                 f'-Название продута: {self.name}.\n'
                 f'-Описание продукта: {self.description}.\n'
@@ -136,17 +212,23 @@ class Smartphone(Product):
 
     def __init__(self, name, description, price,
                  quantity, performance, model, memory, color):
+        """
+         :param name: название продукта.
+         :param description: Описание продукта.
+         :param price: Цена продукта.
+         :param quantity: Количество продуктов в наличии.
+        """
         super().__init__(name, description, price, quantity)
         self.performance = performance
         self.model = model
         self.memory = memory
         self.color = color
 
-    def __repr__(self) -> str:
-        """ Метод для отладки категории Smartphone"""
-
-        return (f'{self.__class__.__name__}: {self.performance},'
-                f'{self.model}, {self.memory}, {self.color}')
+    # def __repr__(self) -> str:
+    #     """ Метод для отладки категории Smartphone"""
+    #
+    #     return (f'{self.__class__.__name__}: {self.performance},'
+    #             f'{self.model}, {self.memory}, {self.color}')
 
 
 class LawnGrass(Product):
@@ -158,45 +240,46 @@ class LawnGrass(Product):
 
     def __init__(self, name, description, price, quantity,
                  manufacturer_country, germination_period, color):
+        """
+         :param name: название продукта.
+         :param description: Описание продукта.
+         :param price: Цена продукта.
+         :param quantity: Количество продуктов в наличии.
+        """
         super().__init__(name, description, price, quantity)
         self.manufacturer_country = manufacturer_country
         self.germination_period = germination_period
         self.color = color
 
-    def __repr__(self) -> str:
-        """ Метод для отладки категории LawnGrass"""
-        return (f'{self.__class__.__name__}: {self.manufacturer_country},'
-                f'{self.germination_period}, {self.color}')
+    # def __repr__(self) -> str:
+    #     """ Метод для отладки категории LawnGrass"""
+    #     return (f'{self.__class__.__name__}: {self.manufacturer_country},'
+    #             f'{self.germination_period}, {self.color}')
 
 
-#  для проверки функции add
+#  для проверки MixinRepr
 if __name__ == '__main__':
     pr1 = Product('Samsung Galaxy C23 Ultra',
                   '256GB, Серый цвет, 200MP камера',
-                  40,
-                  10)
-
-    pr2 = Product('Samsung Galaxy C30 Ultra',
-                  '256GB, Серый цвет, 200MP камера',
-                  10,
-                  10)
-
-    pr3 = Smartphone('Samsung Galaxy C30 Ultra',
-                     '256GB, Серый цвет, 200MP камера',
-                     10,
-                     10,
-                     'max',
-                     '30 as',
+                  18000,
+                  5)
+    pr2 = Smartphone('Iphone 15',
+                     '512GB, Gray space',
+                     21000,
+                     5,
                      250,
+                     'Pro max',
+                     256,
                      'red')
-    pr4 = LawnGrass('трава',
-                    'travka',
-                    100,
-                    5,
+    pr3 = LawnGrass('Трава',
+                    'Газонная трава',
+                    1500,
+                    600,
                     'Russia',
-                    5,
+                    '2 недели',
                     'green')
+    print(pr1.printing)
 
-    print(pr1 + pr2)
-    # print(pr1 + pr3)  # ошибка TypeError
-    # print(pr1 + pr4)  # ошибка TypeError
+    print(pr2.printing)
+
+    print(pr3.printing)
